ae4daa6248b87597d75d336c85b6ddef95be4cdb
Antonio Rubio <46051297+TanqueLab@users.noreply.github.com>
2020-05-24 02:00:25 +0100

    Add files via upload
    class definition, instalisation of objects

---
+++ Person.cpp
@@ -0,0 +1,32 @@
+
+//#include "stdafx.h"
+#include "Person.h"
+#include <iostream>
+
+Person::Person()
+{
+
+}
+
+Person::Person(std::string fName, std::string lName)
+{
+    this->firstName = fName;
+    this->lastName = lName;
+}
+
+Person::Person(std::string fName, std::string lName, int age)
+{
+    this->firstName = fName;
+    this->lastName = lName;
+
+    this->age = age;
+}
+
+
+Person::~Person()
+{
+}
+
+void Person::SayHello()
+{
+}
---
+++ mainPerson.cpp
@@ -0,0 +1,20 @@
+//#include "stdafx.h"
+#include <iostream>
+#include "Person.h"
+
+using namespace std;
+
+int main()
+{
+
+    // create a Person instance using default constructor
+    Person* pOne = new Person();
+
+    // Create a Person instance using 2 argument constructor
+    Person* pTwo = new Person("Tom", "Thumb");
+
+    // Create a Person instance using 3 argument constructor
+    Person* pThree = new Person("Tom", "Thumb", 15);
+
+    return 0;
+}
